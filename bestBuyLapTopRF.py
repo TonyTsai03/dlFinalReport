6d19e0b8e27c18804147d5bd20ab133b61ff0d15
Tony <caizongyou0319@gmail.com>
2024-06-06 16:09:54 +0800

    split data

--- bestBuyLapTopRF.py
+++ bestBuyLapTopRF.py
@@ -5,5 +5,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve
 
+filePath = 'processed_dataset.csv'
+dfCleaning =  pd.read_csv(filePath)
 
-dfCleaning =  pd.read_csv('processed_dataset.csv')
+#target and value
+X = dfCleaning.drop(columns=['aggregateRating/ratingValue'])
+y = dfCleaning['aggregateRating/ratingValue']
+
+#standScaler or labelencoder
+
+#SMOTE
+
+#data split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=1)
+
+#feeding data
+random = 40
+nEstimators = 100
+rfmodel = RandomForestClassifier(n_estimators = nEstimators, random_state = random)
+rfmodel.fit(X_train, y_train)
+
+#testing 
+yPred = rfmodel.predict(X_test)
+yPredProba = rfmodel.predict_proba(X_test)
+
+#data and form
+accuracyValue = accuracy_score(X_test, yPred)
